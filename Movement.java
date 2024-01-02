475358c1dc90a777022e4d4de310fdc776ebb16a
Amalia <111093756+AmaliaEG@users.noreply.github.com>
2024-01-02 14:33:28 +0100

    Update Movement.java

---
+++ Movement.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+java.util.ArrayList;
+
+public class Movement {
+    int velocityX = 0;
+    int velocityY = 0;
+    public static void main(String[] args) {
+
+    }
+
+}
