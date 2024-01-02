d7b2deb603bc4751d2fb42f18fe516de8845e65c
Gabriel Github <101795527+GabrielYassine@users.noreply.github.com>
2024-01-02 12:23:03 +0100

    Grid Fixed

--- Main.java
+++ Main.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 
+import javafx.scene.paint.Color;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sizeInput = new Scanner(System.in);
-        System.out.print("Input a grid size between 5-100 (inclusive): ");
-        int size = sizeInput.nextInt();
-        drawTrack(size);
+        System.out.print("Input a grid width between 5-100 (inclusive): ");
+        int width = sizeInput.nextInt();
+        System.out.print("Input a grid height between 5-100 (inclusive): ");
+        int height = sizeInput.nextInt();
+        drawTrack(width, height);
         sizeInput.close();
     }
 
-    public static void drawTrack(int n) {
-        StdDraw.setXscale(-2*n, 2*n);
-        StdDraw.setYscale(-2*n, 2*n);
-        StdDraw.square(0, 0, 2*n);
-        double dn = n * 2;
-        double corner = -dn + 0.5;
-        for (double i = corner; i < dn; i++) {
-            for (double j = corner; j < dn; j++) {
-                StdDraw.square(i, j, 0.5);
+    public static void drawTrack(int width, int height) {
+        StdDraw.setXscale(0, width);
+        StdDraw.setYscale(0, height);
+        StdDraw.square(0, 0, height);
+        double corner = 0;
+        for (double i = corner; i < width; i++) {
+            for (double j = corner; j < height; j++) {
+                StdDraw.square(i, j, 1);
             }
         }
     }
