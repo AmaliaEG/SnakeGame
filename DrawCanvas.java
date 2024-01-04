f136811ed8c8a505d5990e06684ebca1a71373e5
Gabriel Github <101795527+GabrielYassine@users.noreply.github.com>
2024-01-04 11:20:33 +0100

    Grid fully functional

--- DrawCanvas.java
+++ DrawCanvas.java
@@ -1,18 +1,19 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.Group;
-import javafx.scene.shape.*;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class DrawCanvas extends Application {
 
-    public int MAX_CANVAS = 800;
+    public static final int MAX_CANVAS = 600;
     public int HEIGHT_CANVAS;
     public int WIDTH_CANVAS;
     public int X_GRID_FROM_USER = 5;
-    public int Y_GRID_FROM_USER = 7;
+    public int Y_GRID_FROM_USER = 5;
     public int tileSize;
+    private Stage primaryStage;
 
     public static void main(String[] args) {
         launch(args);
@@ -20,7 +21,7 @@ public class DrawCanvas extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        this.primaryStage = primaryStage;
         calculateCanvasSize();
         createGrid(primaryStage);
     }
@@ -28,25 +29,16 @@ public class DrawCanvas extends Application {
     public void setGridSize(int x, int y) {
         this.X_GRID_FROM_USER = x;
         this.Y_GRID_FROM_USER = y;
-
         calculateCanvasSize();
+        createGrid((Stage) primaryStage.getOwner());
     }
 
     private void calculateCanvasSize() {
+        int maxGridSize = Math.max(X_GRID_FROM_USER, Y_GRID_FROM_USER);
 
-        if (X_GRID_FROM_USER > Y_GRID_FROM_USER) {
-            tileSize = MAX_CANVAS / X_GRID_FROM_USER;
-            WIDTH_CANVAS = MAX_CANVAS;
-            HEIGHT_CANVAS = tileSize * Y_GRID_FROM_USER;
-        } else if (X_GRID_FROM_USER < Y_GRID_FROM_USER) {
-            tileSize = MAX_CANVAS / Y_GRID_FROM_USER;
-            HEIGHT_CANVAS = MAX_CANVAS;
-            WIDTH_CANVAS = tileSize * X_GRID_FROM_USER;
-        } else {
-            tileSize = MAX_CANVAS / X_GRID_FROM_USER;
-            HEIGHT_CANVAS = MAX_CANVAS;
-            WIDTH_CANVAS = MAX_CANVAS;
-        }
+        tileSize = MAX_CANVAS / maxGridSize;
+        WIDTH_CANVAS = tileSize * X_GRID_FROM_USER;
+        HEIGHT_CANVAS = tileSize * Y_GRID_FROM_USER;
     }
 
     private void createGrid(Stage primaryStage) {
@@ -54,28 +46,20 @@ public class DrawCanvas extends Application {
 
         for (int i = 0; i <= X_GRID_FROM_USER; i++) {
             Line lineVertical = new Line(i * tileSize, 0, i * tileSize, HEIGHT_CANVAS);
-
             lineVertical.setStroke(Color.WHITESMOKE);
-
             gridLines.getChildren().add(lineVertical);
         }
 
         for (int i = 0; i <= Y_GRID_FROM_USER; i++) {
             Line lineHorizontal = new Line(0, i * tileSize, WIDTH_CANVAS, i * tileSize);
-
             lineHorizontal.setStroke(Color.WHITESMOKE);
-
             gridLines.getChildren().add(lineHorizontal);
         }
 
-        Group root = new Group(); // Look at the Word Document
+        Group root = new Group();
         Scene scene = new Scene(root, WIDTH_CANVAS, HEIGHT_CANVAS, Color.BLACK);
-
         root.getChildren().add(gridLines);
-
         primaryStage.setScene(scene);
         primaryStage.show();
-    
     }
-
 }
